b9e1a71c7fd1736bd15f9bf1a7ee8e5df2fccdc2
Sho Mizutani <sho@svifa.net>
2025-10-03 20:32:54 +0900

    IIFE

--- src/bookmark.ts
+++ src/bookmark.ts
@@ -1,8 +1,10 @@
-const pick = s => document.querySelector(s)?.children[0]?.textContent?.trim();
-const title = pick("h1.gh-header-title") || pick("div[aria-label=Header] h2");
-const u = new URL(location.href)
-u.hash = ''
-u.search = ''
-if (Boolean(title)) {
- navigator.clipboard.writeText(`[${title}](${u.toString()})`);
-}
+(() => {
+  const pick = s => document.querySelector(s)?.children[0]?.textContent?.trim()
+  const title = pick("h1.gh-header-title") || pick("div[aria-label=Header] h2")
+  const u = new URL(location.href)
+  u.hash = ''
+  u.search = ''
+  if (Boolean(title)) {
+    navigator.clipboard.writeText(`[${title}](${u.toString()})`)
+  }
+})()
