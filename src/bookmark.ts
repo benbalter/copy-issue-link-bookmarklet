bba8b2e684ea9fc7b4d649f33fd5af1b52f20c40
Sho Mizutani <sho@svifa.net>
2025-10-03 21:23:51 +0900

    Remove children[0] and look for bdi, add kind to the title

--- src/bookmark.ts
+++ src/bookmark.ts
@@ -1,10 +1,12 @@
 (() => {
-  const pick = s => document.querySelector(s)?.children[0]?.textContent?.trim()
-  const title = pick("h1.gh-header-title") || pick("div[aria-label=Header] h2")
+  const pick = (s: string) => document.querySelector(s)?.textContent?.trim()
+  // PRs have h1, Issues have h2
+  const title = pick("h1.gh-header-title bdi") || pick("div[aria-label=Header] h2 bdi")
+  if (!title) return
   const u = new URL(location.href)
-  u.hash = ''
   u.search = ''
-  if (Boolean(title)) {
-    navigator.clipboard.writeText(`[${title}](${u.toString()})`)
-  }
+  let suffix = "";
+  const [, kind] = u.hash.match(/^#([a-zA-Z]+)[-_]/) || [];
+  if (kind) suffix = ` (${kind})`;
+  navigator.clipboard.writeText(`[${title}${suffix}](${u.toString()})`)
 })()
