4a47916d39d295ac5f5cf8c8b5005231727e2c03
Sho Mizutani <sho@svifa.net>
2025-10-03 20:29:03 +0900

    pick to pickup title

--- src/bookmark.ts
+++ src/bookmark.ts
@@ -1,16 +1,8 @@
-const titleDiv = document.querySelector("h1.gh-header-title");
-const titleHeader = document.querySelector("div[aria-label=Header] h2");
+const pick = s => document.querySelector(s)?.children[0]?.textContent?.trim();
+const title = pick("h1.gh-header-title") || pick("div[aria-label=Header] h2");
 const u = new URL(location.href)
 u.hash = ''
 u.search = ''
-let title = "";
-
-if (titleDiv && titleDiv.children[0] && titleDiv.children[0].textContent) {
-  title = titleDiv.children[0].textContent.trim();
-} else if (titleHeader && titleHeader.children[0] && titleHeader.children[0].textContent) {
-  title = titleHeader.children[0].textContent.trim();
-}
-
 if (Boolean(title)) {
  navigator.clipboard.writeText(`[${title}](${u.toString()})`);
 }
